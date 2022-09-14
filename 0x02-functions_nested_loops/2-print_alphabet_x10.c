3bc2bd9df2cfcc16ab79a27ffb07a716111b6c6b
Efita Efiom <okokonettaeffiom@yahoo.com>
2022-09-14 14:19:25 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,16 +3,18 @@
 * print_alphabet_x10 - function to print abc 10 times
 *
 * Return: 0
+
 */
 void print_alphabet_x10(void)
-{
+{ 
 	char c, i;
-	
+
 	for (i = 0; i <= 9; i++)
 	{
-		for (c = 'a'; c <= '2'; c++)
-		{
+		for (c = 'a'; c <= 'z'; c++)
+		{	
 			_putchar(c);
 		}
 		_putchar('\n');
+	}
 }
