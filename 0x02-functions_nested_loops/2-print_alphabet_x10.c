6ea1bbe80bc544ca9ee1b362aa3631a46757f8cd
Efita Efiom <okokonettaeffiom@yahoo.com>
2022-09-14 18:44:35 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print alphabelt_x10 - function to print abc 10 times
- * 
- * Return: 0
- */
+* print_alphabet_x10 - function to print abc 10 times
+*
+* Return: 0
+*/
 void print_alphabet_x10(void)
 {
 	char c, i;
@@ -17,4 +17,3 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 	}
 }
-
