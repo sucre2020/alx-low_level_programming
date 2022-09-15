5ba296320211eae506eff63fc864e832d3aeb279
Efita Efiom <okokonettaeffiom@yahoo.com>
2022-09-14 17:19:50 -0700

    8-24_hours.c.swp

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
 * jack_bauer - function that prints every minute of the day of jack bauer
-* starting from 00.00 to 23:59 hours, 
+* it starts from 00:00 to 23:59
+* and resets mins
 * Return: 0
 */
 void jack_bauer(void)
 {
-	int hours = 0;
-	int minutes = 0;
+	int hours =0;
+	int minutes =0;
 	int hours_remainder;
 	int mins_remainder;
 
@@ -18,8 +19,8 @@ void jack_bauer(void)
 	mins_remainder = minutes % 10;
 	hours_remainder = hours % 10;
 	_putchar(hours / 10 + '0');
-	_putchar(hours_remainder + '0')
-	_putchar(':');
+	_putchar(hours_remainder + '0');
+	_putchar(';');
 	_putchar(minutes / 10 + '0');
 	_putchar(mins_remainder + '0');
 	minutes++;
