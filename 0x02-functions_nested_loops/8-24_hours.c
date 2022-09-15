291eca01b476535d20a1a5d0c5efc3b29cb90455
Efita Efiom <okokonettaeffiom@yahoo.com>
2022-09-14 17:24:10 -0700

    8-24_hours.c.swp

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,8 +7,8 @@
 */
 void jack_bauer(void)
 {
-	int hours =0;
-	int minutes =0;
+	int hours = 0;
+	int minutes = 0;
 	int hours_remainder;
 	int mins_remainder;
 
@@ -20,7 +20,7 @@ void jack_bauer(void)
 	hours_remainder = hours % 10;
 	_putchar(hours / 10 + '0');
 	_putchar(hours_remainder + '0');
-	_putchar(';');
+	_putchar(':');
 	_putchar(minutes / 10 + '0');
 	_putchar(mins_remainder + '0');
 	minutes++;
