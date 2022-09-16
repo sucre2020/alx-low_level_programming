2d0312b1d7cd63ff7fb22b2496c57c400d1e0407
Efita Efiom <okokonettaeffiom@yahoo.com>
2022-09-16 16:54:31 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  *print_diagonal - function that draws a diagonal line on the terminal
  *
  *@n: number of times \ should be printed
@@ -31,4 +30,3 @@ void print_diagonal(int n)
 		_putchar('\n');
 	}
 }
-
