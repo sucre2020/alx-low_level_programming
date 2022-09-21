b3fe85eeecef326209a59449da433f89a21dc148
sucre2020 <90860451+sucre2020@users.noreply.github.com>
2022-09-22 00:28:46 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- *leet - function that encode a string
- *@str:string that will be encoded
- *Return:returns encoded string
- */
+* leet - function that encode a string
+*@str:string that will be encoded
+*Return:returns encoded string
+*/
 
 char *leet(char *str)
 {
@@ -16,7 +15,7 @@ char *leet(char *str)
 		for (index2 = 0; index2 <= 7; index2++)
 		{
 			if (str[index1] == leet[index2] ||
-			str[index1] - 32 == leet[index2])
+			 str[index1] - 32 == leet[index2])
 				str[index1] = index2 + '0';
 		}
 	}
