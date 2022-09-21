1d59d9ab7912adc118505297deac802c178b3b35
Efita Efiom <okokonettaeffiom@yahoo.com>
2022-09-21 13:33:03 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *cap_string - function that capitalize first character of a word
- *@str: string to capitalize
+ *@str:string to capitalize
  *Return:returns the capitalized string
  */
 char *cap_string(char *str)
