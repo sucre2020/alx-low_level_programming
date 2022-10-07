69e468612c7eb932c853cdcbb97304a8aa43b108
sucre2020 <90860451+sucre2020@users.noreply.github.com>
2022-10-07 02:58:45 +0100

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,54 +1,51 @@
 #include "header.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- *_realloc - reallocates a memory block using malloc and free
- *
- *@ptr: original pointer to be reallocated
- *@old_size: size of old memory allocation in bytes
- *@new_size: size of new memory allocation in bytes
- *
- *Return: a pointer to the new memory allocation or NULL if the process fails
- */
+  * _realloc - ...
+  * @ptr: ...
+  * @old_size: ...
+  * @new_size: ...
+  *
+  * Return: ...
+  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	int *nptr, *p;
-	unsigned int c;
+	char *nptr;
+	unsigned int i;
 
-	if (old_size == new_size)
-	{
+	if (new_size == old_size)
 		return (ptr);
-	}
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	p = ptr;
-	nptr = malloc(new_size);
-	if (nptr == NULL)
-	{
-		return (NULL);
-	}
+
 	if (ptr == NULL)
 	{
+		nptr = malloc(new_size);
+
+		if (nptr == NULL)
+			return (NULL);
+
 		return (nptr);
-	}
-	if (new_size > old_size)
-	{
-		for (c = 0; c < old_size; c++)
-		{
-			nptr[c] = p[c];
-		}
 	}
 	else
 	{
-		for (c = 0; c < new_size; c++)
+		if (new_size == 0)
 		{
-			nptr[c] = p[c];
+			free(ptr);
+			return (NULL);
 		}
 	}
+
+	nptr = malloc(new_size);
+
+	if (nptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size && i < new_size; i++)
+	{
+		nptr[i] = ((char *) ptr)[i];
+	}
+
 	free(ptr);
 	return (nptr);
 }
